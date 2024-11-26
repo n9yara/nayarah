cadab1a06838110352275bae7b744c87248d2f8e
n9yara <zdelucuaffg@gmail.com>
2024-11-25 23:40:30 -0300

    nayara

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Obter os elementos do DOM
+
 const overlay = document.getElementById('overlay');
 const content = document.getElementById('content');
 
-// Função para revelar o conteúdo ao clicar em qualquer lugar da tela
+
 overlay.addEventListener('click', function() {
-    // Esconder o overlay e mostrar o conteúdo
+   
     overlay.style.display = 'none';
     content.style.display = 'block';
 });
